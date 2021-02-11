03c66d668f366b388d2fd11dca8ca0c6685647df
Raj Kale <kale44772@gmail.com>
2021-02-11 22:41:01 +0530

    initial

---
+++ pages/eom.js
@@ -0,0 +1,11 @@
+export const EOM=()=>{
+    return(
+        <div className="page-container">
+            <div>
+                <h1>Employee of the year</h1>
+            </div>
+        </div>
+    )
+}
+
+export default EOM;
