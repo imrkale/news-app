8cba9a0439633f86944c1603edfe58661e03ee45
Raj Kale <kale44772@gmail.com>
2021-02-14 00:00:49 +0530

    final commit

--- pages/eom.js
+++ pages/eom.js
@@ -1,11 +1,60 @@
-export const EOM=()=>{
+import Head from 'next/head'
+import { Toolbar } from '../components/toolbar';
+import styles from '../styles/eom.module.css'
+export const EOM=({employee})=>{
+    console.log(employee);
     return(
-        <div className="page-container">
-            <div>
-                <h1>Employee of the year</h1>
-            </div>
+        <>
+        <Head>
+        <title>Employee Of The Month</title>
+        <meta
+          name="description"
+          content={`This month's employee of the month is ${employee.name}`}
+        />
+
+        <meta property="og:image" content={employee.image} />
+        <meta property="og:title" content="Employee Of The Month" />
+        <meta
+          property="og:description"
+          content={`This month's employee of the month is ${employee.name}`}
+        />
+
+        <meta property="twitter:image" content={employee.image} />
+        <meta property="twitter:title" content="Employee Of The Month" />
+        <meta
+          property="twitter:description"
+          content={`This month's employee of the month is ${employee.name}`}
+        />
+      </Head>
+      <Toolbar/>
+      <div className="page-container">
+        
+        <div className={styles.main}>
+          <h1>Employee Of The Month</h1>
+
+          <div className={styles.employeeOfTheMonth}>
+            <h3>{employee.name}</h3>
+            <h6>{employee.Profession}</h6>
+            <img src={employee.image} alt="employee" />
+            <p>{employee.Education}</p>
+          </div>
         </div>
+      </div>
+      </>
     )
 }
 
-export default EOM;
+
+export const getServerSideProps = async pageContext => {
+    const apiResponse = await fetch(
+      'https://my-json-server.typicode.com/imrkale/news-app/employeeDB',
+    );
+    const employee = await apiResponse.json();
+  
+    return {
+      props: {
+        employee,
+      },
+    };
+  };
+  export default EOM;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
+import {Toolbar} from '../components/toolbar'
 export default function Home() {
   return (
-    <div>Raj Kale</div>
+    <div className="page-container">
+      <Toolbar />
+
+      <div className={styles.main}>
+        <h1>Next.js News App</h1>
+        <form action="api/hello" method="POST">
+          <input type="text" name="name" id="name" placeholder="Enter name"/>
+          <button type="submit">Submit</button>
+        </form>
+
+        <h3>Your one stop shop for the latest news articles</h3>
+      </div>
+    </div>
   )
 }
